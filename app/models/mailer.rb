89d916382e5a51cf5589955834b8e1a1a8aad925
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2018-04-17 13:42:07 -0500

    mailers send to all users when date is = to Date.today

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -3,10 +3,10 @@ class Mailer < ApplicationRecord
     after_validation :send_email
 
     def send_email()
-        Mailer.all.each do |mailer|
-        if mailer.itself.last_date == Date.today #In this case, 'if true' would do exactly the same!
+        if Mailer.where('last_date <= ?', Date.today)
             ExampleMailer.sample_email().deliver 
         end
     end
+  
 end 
-end 
+        
