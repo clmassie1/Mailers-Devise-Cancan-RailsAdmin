1d84f61af23da832469e5670190c6a3885eec8fb
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2018-04-23 17:27:23 -0500

    Query data in graphs

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,23 +1,48 @@
 class Mailer < ApplicationRecord
     include RailsAdminCharts
 
-    def self.graph_data since=30.days.ago
-        [
-          {
-              name: 'Admin Users',
-              pointInterval: point_interval = 1.day * 1000,
-              pointStart: start_point = since.to_i * 1000,
-              data: self.where(type: 'Admin').delta_records_since(since)
-          },
-          {
-              name: 'Standard Users',
-              pointInterval: point_interval,
-              pointStart: start_point,
-              data: self.where(type: nil).delta_records_since(since)
-          }
-        ]
-        
+    # def self.graph_data since=30.days.ago
+    #     [
+    #       {
+    #           name: 'Resource with Hardware',
+    #           pointInterval: point_interval = 1.day * 1000,
+    #           pointStart: start_point = since.to_i * 1000,
+    #           data: self.where(last_date: Date.today).delta_records_since(since)
+    #       },
+    #       {
+    #           name: 'Resource with Software',
+    #           pointInterval: point_interval,
+    #           pointStart: start_point,
+    #           data: self.where(first_date: nil).delta_records_since(since)
+    #       },{
+    #         name: 'Resource with Hardware',
+    #         pointInterval: point_interval = 1.day * 1000,
+    #         pointStart: start_point = since.to_i * 1000,
+    #         data: self.where(last_date: Date.today).delta_records_since(since)
+    #     }
+    #     ]
+    # end
+        def self.graph_data(since = 30.days.ago)
+
+         Mailer.where(last_date: nil).delta_records_since(since)
+          # Output
+          # [["Bagmati", 3], ["Gandaki", 3], ["Janakpur", 5]]
+        end
+     
+
+  
+
+      def self.chart_type
+        'pie'
       end
+    #   def self.label_rotation
+    #         "-45"
+    #       end
+
+    #       def self.xaxis
+    #         ['last_date', 'cat b', 'cat c' 'cat d', 'cat e', 'cat f', 'cat g', 'cat h']
+    #       end
+
         after_create :send_email_if 
         
         def send_email_if
